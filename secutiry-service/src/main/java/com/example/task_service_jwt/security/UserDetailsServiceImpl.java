12003fa0d5f90605a6b795dc1d57e315f1df8317
Maxim <batman-star-pro@mail.ru>
2024-09-06 13:28:12 +0700

    jwt security

---
+++ secutiry-service/src/main/java/com/example/task_service_jwt/security/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.example.task_service_jwt.security;
+
+import com.example.task_service_jwt.entity.User;
+import com.example.task_service_jwt.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    //instead username I use only email
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(()->new UsernameNotFoundException("user not found. username is:\s" + username));
+        return new AppUserDetails(user);
+    }
+}
